48efaf87739f872c9982b1366804c4e5739b9c28
cartomantedijotape <cartomantedijotape@gmail.com>
2026-04-14 22:13:53 -0300

    remove wpp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function HomePage() {
       </main>
 
       <Footer />
-      <WhatsAppButton />
+      {/*<WhatsAppButton />*/}
       <SocialProofToast />
     </>
   );
--- src/app/produto/[slug]/page.tsx
+++ src/app/produto/[slug]/page.tsx
@@ -81,7 +81,7 @@ export default async function ProductPage({ params }: PageProps) {
         )}
       </main>
       <Footer />
-      <WhatsAppButton />
+      {/*<WhatsAppButton />*/}
     </>
   );
 }
